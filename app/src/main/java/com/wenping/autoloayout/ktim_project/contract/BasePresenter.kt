62d9acc81d0a974e0aa6d2f53df07c7b1137760e
kk <wenpingkk@163.com>
2018-04-08 23:20:43 +0800

    003 SplashContract

---
+++ app/src/main/java/com/wenping/autoloayout/ktim_project/contract/BasePresenter.kt
@@ -0,0 +1,12 @@
+package com.wenping.autoloayout.ktim_project.contract
+
+/**
+ * Author WenPing
+ * CreateTime 2018/4/8.
+ * Description:
+ */
+interface BasePresenter {
+
+
+
+}
---
+++ app/src/main/java/com/wenping/autoloayout/ktim_project/contract/SplashContract.kt
@@ -0,0 +1,23 @@
+package com.wenping.autoloayout.ktim_project.contract
+
+/**
+ * Author WenPing
+ * CreateTime 2018/4/8.
+ * Description:
+ */
+interface SplashContract {
+
+    interface Presentern : BasePresenter {
+        //检查是否登陆
+        fun checkLoginStatus()
+
+    }
+    interface View{
+        //没登录
+        fun onNotLoginedIn()
+
+        //已登陆
+        fun onLoginedIn()
+
+    }
+}
