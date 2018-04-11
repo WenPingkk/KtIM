50bce64b7584d652832e8a5d9200068e629111bd
kk <wenpingkk@163.com>
2018-04-12 00:05:57 +0800

    013 注册界面 View层的实现

--- app/src/main/java/com/wenping/autoloayout/ktim_project/RegisterActivity.kt
+++ app/src/main/java/com/wenping/autoloayout/ktim_project/RegisterActivity.kt
@@ -1,11 +1,45 @@
 package com.wenping.autoloayout.ktim_project
 
+import com.wenping.autoloayout.ktim_project.contract.RegisterContract
+import kotlinx.android.synthetic.main.activity_register.*
+import org.jetbrains.anko.toast
+
 /**
  * Author WenPing
  * CreateTime 2018/4/11.
  * Description:
  */
-class RegisterActivity : BaseActivity() {
+class RegisterActivity : BaseActivity() ,RegisterContract.View{
+
+
+    override fun onUserNameError() {
+
+        userName.error = getString(R.string.user_name_error)
+
+    }
+
+    override fun onPasswodError() {
+        password.error = getString(R.string.password_error)
+    }
+
+    override fun onConfirmPasswordError() {
+        confirmPassword.error = getString(R.string.confirm_password_error)
+    }
+
+    override fun onStartRegister() {
+        showProgressDialog(getString(R.string.registering))
+    }
+
+    override fun onRegisterSuccess() {
+        dissProgressDialog()
+
+        finish()
+    }
+
+    override fun onRegisterFailedO() {
+        dissProgressDialog()
+        toast(R.string.register_failed)
+    }
 
     override fun getLayoutId(): Int  = R.layout.activity_register
 
