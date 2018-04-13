54c5b2ceb7e9f023f569c567c0196b3888261333
kk <wenpingkk@163.com>
2018-04-13 22:17:01 +0800

    015 bmob sdk 集成

--- app/src/main/java/com/wenping/autoloayout/ktim_project/app/GlobalApplication.kt
+++ app/src/main/java/com/wenping/autoloayout/ktim_project/app/GlobalApplication.kt
@@ -1,6 +1,7 @@
 package com.wenping.autoloayout.ktim_project.app
 
 import android.app.Application
+import cn.bmob.v3.Bmob
 import com.hyphenate.chat.EMClient
 import com.hyphenate.chat.EMOptions
 import com.wenping.autoloayout.ktim_project.BuildConfig
@@ -23,6 +24,8 @@ class GlobalApplication : Application() {
         EMClient.getInstance().init(applicationContext, EMOptions());
         //在做打包混淆时，关闭debug模式，避免消耗不必要的资源
         EMClient.getInstance().setDebugMode(BuildConfig.DEBUG);
+
+        Bmob.initialize(applicationContext, "2e91983d054f9b525e9fd550052a3c56")
     }
 
 }
