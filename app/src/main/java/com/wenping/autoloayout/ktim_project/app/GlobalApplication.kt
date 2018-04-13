aff0bc42c278fbe980b4d5e5b2e3612a84ffbc3d
kk <wenpingkk@163.com>
2018-04-13 22:58:04 +0800

    016 注冊到bmob

--- app/src/main/java/com/wenping/autoloayout/ktim_project/app/GlobalApplication.kt
+++ app/src/main/java/com/wenping/autoloayout/ktim_project/app/GlobalApplication.kt
@@ -13,19 +13,20 @@ import com.wenping.autoloayout.ktim_project.BuildConfig
  */
 class GlobalApplication : Application() {
 
-    override fun onCreate() {
-        super.onCreate()
-        initHuanXin()
+    companion object {
+        lateinit var instance:GlobalApplication
     }
 
-    private fun initHuanXin() {
 
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
         //初始化
-        EMClient.getInstance().init(applicationContext, EMOptions());
+        EMClient.getInstance().init(applicationContext, EMOptions())
         //在做打包混淆时，关闭debug模式，避免消耗不必要的资源
-        EMClient.getInstance().setDebugMode(BuildConfig.DEBUG);
+        EMClient.getInstance().setDebugMode(BuildConfig.DEBUG)
 
-        Bmob.initialize(applicationContext, "2e91983d054f9b525e9fd550052a3c56")
+        Bmob.initialize(applicationContext, "ef808b3b36ee22248d19f04287ce5b37")
     }
 
 }
--- app/src/main/java/com/wenping/autoloayout/ktim_project/presenter/RegisterPresenter.kt
+++ app/src/main/java/com/wenping/autoloayout/ktim_project/presenter/RegisterPresenter.kt
@@ -1,5 +1,8 @@
 package com.wenping.autoloayout.ktim_project.presenter
 
+import cn.bmob.v3.BmobUser
+import cn.bmob.v3.exception.BmobException
+import cn.bmob.v3.listener.SaveListener
 import com.wenping.autoloayout.ktim_project.contract.RegisterContract
 import com.wenping.autoloayout.ktim_project.extentions.isValidPassword
 import com.wenping.autoloayout.ktim_project.extentions.isValidUserName
@@ -19,8 +22,34 @@ class RegisterPresenter(val view:RegisterContract.View) :RegisterContract.Presen
                 if (confirmPassword.equals(password)) {
                     //密码和确认密码一支
                     view.onStartRegister()
+
+                    //开始注册
+                    registerBmob(userName,password)
+
                 }else view.onConfirmPasswordError()
             }else view.onPasswodError()
         }else view.onUserNameError()
+    }
+
+    private fun registerBmob(userName: String, password: String) {
+
+        val bu = BmobUser()
+        bu.username = userName
+        bu.setPassword(password)
+
+        //注意 不能用save方法进行注册
+        bu.signUp<BmobUser>(object:SaveListener<BmobUser>(){
+            override fun done(p0: BmobUser?, e: BmobException?) {
+                if (e == null) {
+                    //注册成功
+
+                    //注册到环信
+                } else {
+                    //注册失败
+                    view.onRegisterFailedO()
+                }
+            }
+        })
+
     }
 }
