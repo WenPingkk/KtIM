1afe879e64bfff0b3a1a39d84a1ef5318c5b9842
kk <wenpingkk@163.com>
2018-04-14 13:18:42 +0800

    021 联系人界面 recycler初始化

---
+++ app/src/main/java/com/wenping/autoloayout/ktim_project/widget/ContactListIgtemView.kt
@@ -0,0 +1,23 @@
+package com.wenping.autoloayout.ktim_project.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.RelativeLayout
+import com.wenping.autoloayout.ktim_project.R
+
+/**
+ * Author WenPing
+ * CreateTime 2018/4/14.
+ * Description:
+ */
+class ContactListIgtemView(context: Context?, attrs: AttributeSet?=null) : RelativeLayout(context, attrs) {
+
+    init {
+        //布局创建把这个布局添加到组合式自定义布局
+        View.inflate(context, R.layout.view_contact_item,this)
+
+
+    }
+
+}
