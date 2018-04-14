9ecc3aa256d5d554868a685117814cde758d29b8
kk <wenpingkk@163.com>
2018-04-14 13:13:15 +0800

    020 联系人界面 初始化

--- app/src/main/java/com/wenping/autoloayout/ktim_project/ui/fragment/ContactFragment.kt
+++ app/src/main/java/com/wenping/autoloayout/ktim_project/ui/fragment/ContactFragment.kt
@@ -1,7 +1,10 @@
 package com.wenping.autoloayout.ktim_project.ui.fragment
 
+import android.view.View
 import com.wenping.autoloayout.ktim_project.R
 import com.wenping.autoloayout.ktim_project.ui.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_contacts.*
+import kotlinx.android.synthetic.main.header.*
 
 /**
  * Author WenPing
@@ -10,5 +13,19 @@ import com.wenping.autoloayout.ktim_project.ui.base.BaseFragment
  */
 class ContactFragment : BaseFragment() {
     override fun getLayoutId(): Int = R.layout.fragment_contacts
-    
+
+    override fun init() {
+        super.init()
+        headerTitle.text = getString(R.string.contact)
+        add.visibility = View.VISIBLE
+
+        /**
+         * apply的方法
+         */
+        swipeRefreshLayout.apply {
+            setColorSchemeResources(R.color.qq_blue)
+            isRefreshing = true
+        }
+
+    }
 }
