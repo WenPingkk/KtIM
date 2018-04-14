0ffb6041ca420d49b76992887745bb1e86c47a67
kk <wenpingkk@163.com>
2018-04-14 18:56:24 +0800

    026 绘制slidebar

---
+++ app/src/main/java/com/wenping/autoloayout/ktim_project/widget/Slidebar.kt
@@ -0,0 +1,69 @@
+package com.wenping.autoloayout.ktim_project.widget
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import com.wenping.autoloayout.ktim_project.R
+import org.jetbrains.anko.sp
+
+/**
+ * Author WenPing
+ * CreateTime 2018/4/14.
+ * Description:
+ */
+class Slidebar(context: Context, attrs: AttributeSet?=null) : View(context, attrs) {
+
+    var sectionHeight = 0f
+    var textBaseLine = 0f
+    var onSectionChangeListener:OnSectionChangeListener?=null
+
+    var paint = Paint()
+
+    companion object {
+        private val SECTIONS = arrayOf("A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+                "K", "L","M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+    }
+
+    init {
+        paint.apply {
+            color = resources.getColor(R.color.qq_section_text_color)
+            textSize= sp(12).toFloat()
+
+            textAlign = Paint.Align.CENTER
+        }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        //计算么个字符分配的高度
+        sectionHeight = h*1.0f/ SECTIONS.size
+        val fonrtMetric = paint.fontMetrics
+        //计算绘制文本的高度
+        val textHeight = fonrtMetric.descent-fonrtMetric.ascent
+        //计算基准线
+        textBaseLine = sectionHeight/2+(textHeight/2-fonrtMetric.descent)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        //绘制字符的起始位置
+        val x = width*1.0f/2
+        var baseLine = textBaseLine
+        SECTIONS.forEach {
+            canvas.drawText(it,x,baseLine,paint)
+            //更新，绘制下一个字符
+            baseLine+=sectionHeight
+        }
+
+    }
+
+    interface OnSectionChangeListener {
+
+        fun onSectionChange(frstLetter:String)
+
+        fun onSlideFinish()
+    }
+
+}
