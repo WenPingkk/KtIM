ee11d3a3981738a3c592d345856708f33c514d56
wenping <wenpingkk@163.com>
2018-04-18 10:51:22 +0800

    035 添加好友view层实现

--- app/src/main/java/com/wenping/autoloayout/ktim_project/ui/activity/AddFriendsActivity.kt
+++ app/src/main/java/com/wenping/autoloayout/ktim_project/ui/activity/AddFriendsActivity.kt
@@ -3,9 +3,11 @@ package com.wenping.autoloayout.ktim_project.ui.activity
 import android.support.v7.widget.LinearLayoutManager
 import com.wenping.autoloayout.ktim_project.R
 import com.wenping.autoloayout.ktim_project.adapter.AddFriendListFriendAdapter
+import com.wenping.autoloayout.ktim_project.contract.AddFriendContract
 import com.wenping.autoloayout.ktim_project.ui.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_add_friend.*
 import kotlinx.android.synthetic.main.header.*
+import org.jetbrains.anko.toast
 
 /**
  * @author WenPing
@@ -13,7 +15,8 @@ import kotlinx.android.synthetic.main.header.*
  * @decription:
  *<p>
  */
-class AddFriendsActivity : BaseActivity() {
+class AddFriendsActivity : BaseActivity() ,AddFriendContract.View{
+
     override fun getLayoutId(): Int = R.layout.activity_add_friend
 
     override fun init() {
@@ -29,5 +32,16 @@ class AddFriendsActivity : BaseActivity() {
         }
     }
 
+    override fun onSeachSuccess() {
+        dissProgressDialog()
+        toast(R.string.search_success)
+        //通知recyclerview刷新界面
+        recyclerView.adapter.notifyDataSetChanged()
+    }
+
+    override fun onSeachFailed() {
+        dissProgressDialog()
+        toast(R.string.search_failed)
+    }
 
 }
