be42eb0b643cadee924817bae43a391dd2967bbf
wenping <wenpingkk@163.com>
2018-04-18 11:08:24 +0800

    036 presenter的实现

---
+++ app/src/main/java/com/wenping/autoloayout/ktim_project/presenter/AddFriendPresenter.kt
@@ -0,0 +1,18 @@
+package com.wenping.autoloayout.ktim_project.presenter
+
+import com.wenping.autoloayout.ktim_project.contract.AddFriendContract
+
+/**
+ * @author WenPing
+ * @date 2018/4/18
+ * @decription:
+ *<p>
+ */
+class AddFriendPresenter(var view:AddFriendContract.View) :AddFriendContract.Presenter{
+
+
+
+    override fun search(key: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
--- app/src/main/java/com/wenping/autoloayout/ktim_project/ui/activity/AddFriendsActivity.kt
+++ app/src/main/java/com/wenping/autoloayout/ktim_project/ui/activity/AddFriendsActivity.kt
@@ -1,9 +1,12 @@
 package com.wenping.autoloayout.ktim_project.ui.activity
 
 import android.support.v7.widget.LinearLayoutManager
+import android.view.KeyEvent
+import android.widget.TextView
 import com.wenping.autoloayout.ktim_project.R
 import com.wenping.autoloayout.ktim_project.adapter.AddFriendListFriendAdapter
 import com.wenping.autoloayout.ktim_project.contract.AddFriendContract
+import com.wenping.autoloayout.ktim_project.presenter.AddFriendPresenter
 import com.wenping.autoloayout.ktim_project.ui.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_add_friend.*
 import kotlinx.android.synthetic.main.header.*
@@ -16,6 +19,8 @@ import org.jetbrains.anko.toast
  *<p>
  */
 class AddFriendsActivity : BaseActivity() ,AddFriendContract.View{
+
+    val presenter = AddFriendPresenter(this)
 
     override fun getLayoutId(): Int = R.layout.activity_add_friend
 
@@ -30,6 +35,22 @@ class AddFriendsActivity : BaseActivity() ,AddFriendContract.View{
             adapter = AddFriendListFriendAdapter(context)
 
         }
+
+        search.setOnClickListener{
+            search()
+        }
+        userName.setOnEditorActionListener { v, actionId, event ->
+            search()
+            true
+        }
+    }
+
+    //搜索操作
+    fun search() {
+        hideSoftKeyBoard()
+        showProgressDialog(getString(R.string.searching))
+        val key = userName.text.trim().toString()
+        presenter.search(key)
     }
 
     override fun onSeachSuccess() {
