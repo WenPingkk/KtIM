900866265cb3799ab4b17fd15bf406800766b6e8
wenping <wenpingkk@163.com>
2018-04-18 13:56:25 +0800

    039 Contact实体类

---
+++ app/src/main/java/com/wenping/autoloayout/ktim_project/data/db/Contact.kt
@@ -0,0 +1,13 @@
+package com.wenping.autoloayout.ktim_project.data.db
+
+/**
+ * @author WenPing
+ * @date 2018/4/18
+ * @decription:
+ *<p>
+ */
+data class Contact(val map:MutableMap<String,Any?>) {
+
+    val _id by map
+    val name by map
+}
