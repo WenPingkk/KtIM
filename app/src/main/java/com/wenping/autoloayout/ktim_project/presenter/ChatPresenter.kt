51d48220a7b02586800eb37ff63bc913fcb39ab2
wenping <wenpingkk@163.com>
2018-04-20 12:27:56 +0800

    049 SendMessage model层的实现;

--- app/src/main/java/com/wenping/autoloayout/ktim_project/presenter/ChatPresenter.kt
+++ app/src/main/java/com/wenping/autoloayout/ktim_project/presenter/ChatPresenter.kt
@@ -1,5 +1,8 @@
 package com.wenping.autoloayout.ktim_project.presenter
 
+import com.hyphenate.chat.EMClient
+import com.hyphenate.chat.EMMessage
+import com.wenping.autoloayout.ktim_project.adapter.EMCallBackAdapter
 import com.wenping.autoloayout.ktim_project.contract.ChatContract
 
 /**
@@ -10,7 +13,23 @@ import com.wenping.autoloayout.ktim_project.contract.ChatContract
  */
 class ChatPresenter(val view:ChatContract.View) :ChatContract.ChatPresenter{
 
+    val messages = mutableListOf<EMMessage>()
+
     override fun sendMessage(contact: String, message: String) {
 
+        val emMessage = EMMessage.createTxtSendMessage(message,contact)
+        emMessage.setMessageStatusCallback(object :EMCallBackAdapter(){
+            override fun onSuccess() {
+                //这是在子线程中
+               uiThread {  view.onSendMessageSuccess() }
+            }
+
+            override fun onError(p0: Int, p1: String?) {
+                uiThread {  view.onSendMessageFailed() }
+            }
+        })
+        messages.add(emMessage)
+        view.onStartSendMessage()
+        EMClient.getInstance().chatManager().sendMessage(emMessage)
     }
 }
