f45d922dd1db24a6597c07cdcd2c702161f36be9
kk <wenpingkk@163.com>
2018-04-22 15:13:36 +0800

    058 显示消息列表，未读消息

--- app/src/main/java/com/wenping/autoloayout/ktim_project/widget/ConversationItemView.kt
+++ app/src/main/java/com/wenping/autoloayout/ktim_project/widget/ConversationItemView.kt
@@ -5,7 +5,12 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.RelativeLayout
 import com.hyphenate.chat.EMConversation
+import com.hyphenate.chat.EMMessage
+import com.hyphenate.chat.EMTextMessageBody
+import com.hyphenate.util.DateUtils
 import com.wenping.autoloayout.ktim_project.R
+import kotlinx.android.synthetic.main.view_conversation_item.view.*
+import java.util.*
 
 /**
  * Author WenPing
@@ -18,6 +23,23 @@ class ConversationItemView(context: Context?, attrs: AttributeSet? = null) : Rel
     }
 
     fun bindView(emConversation: EMConversation) {
+        userName.text = emConversation.conversationId()
+        if (emConversation.lastMessage.type == EMMessage.Type.TXT) {
+            val emTextMessageBody = emConversation.lastMessage.body as EMTextMessageBody
+            lastMessage.text = emTextMessageBody.message
+        } else {
+            lastMessage.text = context.getString(R.string.no_text_message)
+        }
+        val timestampString = DateUtils
+                .getTimestampString(Date(emConversation.lastMessage.msgTime))
+        timestamp.text = timestampString
+
+        if (emConversation.unreadMsgCount > 0) {
+            unreadCount.visibility = View.VISIBLE
+            unreadCount.text = emConversation.unreadMsgCount.toString()
+        } else {
+            unreadCount.visibility = View.GONE
+        }
 
 
 
